7a845f9c85248c808a4bafb53cd025fa55032123
unknown <supejunior25251325@gmail.com>
2020-10-07 19:19:08 +0700

    part 1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Volunteers.Models.Entity;
+
+namespace Volunteers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VolunteerEntities db = new VolunteerEntities();
+        public ActionResult Index()
+        {
+            ViewBag.LstVolunteer = db.Round_Volunteer.ToList();
+            return View();
+        }
+
+        public ActionResult AddVolunteer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Round_Volunteer entity)
+        {
+            try
+            {
+                var model = new Round_Volunteer();
+                model.CreatedDate = DateTime.Now;
+                model.Place = entity.Place;
+                model.StartDate = entity.StartDate;
+                model.EndDate = entity.EndDate;
+                model.Register = entity.Register;
+                model.Status = true;
+                db.Round_Volunteer.Add(model);
+                db.SaveChanges();
+                TempData["add_success"] = "Thêm đợt tình nguyện thành công";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("AddVolunteer");
+            }
+        }
+
+        public ActionResult EditVolunteer(long ID)
+        {
+            ViewBag.Round_Volunteer = db.Round_Volunteer.Find(ID);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Round_Volunteer entity)
+        {
+            try
+            {
+                var model = db.Round_Volunteer.Find(entity.ID);
+                model.Place = entity.Place;
+                model.StartDate = entity.StartDate;
+                model.EndDate = entity.EndDate;
+                model.Register = entity.Register;
+                db.SaveChanges();
+                TempData["add_success"] = "Sửa đợt tình nguyện thành công";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                TempData["add_success"] = "Sửa KHÔNG thành công";
+                return RedirectToAction("Index");
+            }
+        }
+
+        public JsonResult Delete(long ID)
+        {
+            var rv = db.Round_Volunteer.Find(ID);
+            db.Round_Volunteer.Remove(rv);
+            db.SaveChanges();
+            return Json(new
+            {
+                status = true
+            });
+        }
+    }
+}
---
+++ Controllers/VolunteerController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Volunteers.Models.Entity;
+
+namespace Volunteers.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        private VolunteerEntities db = new VolunteerEntities();
+        // GET: Volunteer
+        public ActionResult Index()
+        {
+            ViewBag.lstVolunteers = db.Volunteers.Where(x => x.Type == 1).ToList();
+            return View();
+        }
+        
+
+        //Thêm tình nguyện viên
+        public ActionResult AddVolunteer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddVolunteer(Volunteer entity)
+        {
+            try
+            {
+                var model = new Volunteer();
+                model.Fullname = entity.Fullname;
+                model.Birthday = entity.Birthday;
+                model.Phone = entity.Phone;
+                model.Email = entity.Email;
+                model.Address = entity.Address;
+                model.Type = 1;
+                model.RoleName = entity.RoleName;
+                db.Volunteers.Add(model);
+                db.SaveChanges();
+                TempData["add_success"] = "Thêm tình nguyện viên thành công";
+                return Redirect("/Volunteer/Index");
+            }
+            catch
+            {
+                TempData["add_success"] = "Thêm tình nguyện viên KHÔNG thành công";
+                return Redirect("/Volunteer/Index");
+            }
+        }
+
+        //Sửa tình nguyện viên
+        public ActionResult EditVolunteer(long ID)
+        {
+            ViewBag.volunteer = db.Volunteers.Find(ID);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditVolunteer(Volunteer entity)
+        {
+            try
+            {
+                var model = db.Volunteers.Find(entity.ID);
+                model.Fullname = entity.Fullname;
+                model.Birthday = entity.Birthday;
+                model.Phone = entity.Phone;
+                model.Email = entity.Email;
+                model.Address = entity.Address;
+                model.RoleName = entity.RoleName;
+                db.SaveChanges();
+                TempData["add_success"] = "Sửa tình nguyện viên thành công";
+                return Redirect("/Volunteer/Index");
+            }
+            catch
+            {
+                TempData["add_success"] = "Sửa tình nguyện viên KHÔNG thành công";
+                return Redirect("/Volunteer/Index");
+            }
+        }
+
+        //Xóa tình nguyện viên
+        public JsonResult DeleteVolunteer(long ID)
+        {
+            var vol = db.Volunteers.Find(ID);
+            db.Volunteers.Remove(vol);
+            db.SaveChanges();
+            return Json(new
+            {
+                status = true
+            });
+        }
+
+        //Mạnh thường quân
+        public ActionResult Person()
+        {
+            ViewBag.lstVolunteers = db.Volunteers.Where(x => x.Type == 2).ToList();
+            return View();
+        }
+
+        //Thêm mạnh thường quân
+        public ActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(Volunteer entity)
+        {
+            try
+            {
+                var model = new Volunteer();
+                model.Fullname = entity.Fullname;
+                model.Birthday = entity.Birthday;
+                model.Phone = entity.Phone;
+                model.Email = entity.Email;
+                model.Address = entity.Address;
+                model.Type = 2;
+                db.Volunteers.Add(model);
+                db.SaveChanges();
+                TempData["add_success"] = "Thêm mạnh thường quân thành công";
+                return Redirect("/Volunteer/Person");
+            }
+            catch
+            {
+                TempData["add_success"] = "Thêm mạnh thường quân KHÔNG thành công";
+                return Redirect("/Volunteer/Person");
+            }
+        }
+
+        //Sửa mạnh thường quân
+        public ActionResult EditPerson(long ID)
+        {
+            ViewBag.volunteer = db.Volunteers.Find(ID);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditPerson(Volunteer entity)
+        {
+            try
+            {
+                var model = db.Volunteers.Find(entity.ID);
+                model.Fullname = entity.Fullname;
+                model.Birthday = entity.Birthday;
+                model.Phone = entity.Phone;
+                model.Email = entity.Email;
+                model.Address = entity.Address;
+                db.SaveChanges();
+                TempData["add_success"] = "Sửa mạnh thường quân thành công";
+                return Redirect("/Volunteer/Person");
+            }
+            catch
+            {
+                TempData["add_success"] = "Sửa mạnh thường quân KHÔNG thành công";
+                return Redirect("/Volunteer/Person");
+            }
+        }
+
+        //Xóa mạnh thường quân
+        public JsonResult DeletePerson(long ID)
+        {
+            var vol = db.Volunteers.Find(ID);
+            db.Volunteers.Remove(vol);
+            db.SaveChanges();
+            return Json(new
+            {
+                status = true
+            });
+        }
+    }
+}
